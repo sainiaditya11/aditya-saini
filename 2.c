9d2c23ff12adad8ec0187517ecc7c45bdc1eceee
sainiaditya11 <adirya9079@gmail.com>
2026-02-22 18:11:29 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+
+    for(i = 0; i < n-1; i++) {
+        minIndex = i;
+
+        for(j = i+1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+}
+
+int main() {
+    int arr[] = {29, 10, 14, 37, 13};
+    int n = 5;
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
